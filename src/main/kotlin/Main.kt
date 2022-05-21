a86d1fabd1ba6302b471c1d4e016090e6afc388b
Sohail1011 <105809945+Sohail1011@users.noreply.github.com>
2022-05-21 18:20:36 -0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 fun main() {
-    val items = listOf("brocoli", "tomato", "carrot")
-    var index = 0
-    while (index < items.size) {
-        println("Item at $index is ${items[index]}")
-        index++
-    }
+    println(describe(2))
 }
+
+fun describe(obj: Any): String =
+    when (obj) {
+        1 -> "One"
+        "Hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown"
+    }
 
 // This is a code for the future
