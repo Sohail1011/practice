2a7357bee585215d505130de2b5354b4d52670f1
Shelly <aratavalga@gmail.com>
2022-05-21 16:36:16 -0500

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    printSum(2, -5)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun printSum(a: Int, b: Int): Unit {
+    println("Suma de $a y $b dan como resultado ${a + b}")
 }
