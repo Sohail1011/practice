69a029c516eeac05d4ffb31bc7233cf342d22de9
Sohail1011 <105809945+Sohail1011@users.noreply.github.com>
2022-05-21 17:18:22 -0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
+val PI = 3.14
+var x = 0
+fun incrementX() {
+    x += 1
+}
+
 fun main() {
-    val a: Int = 1
-    val b = 7
-    val c: Int
-    c = 3
-    println("${a + b + c}")
+    println("x = $x, PI = $PI")
+    incrementX()
+    println("incrementX()")
+    println("x = $x, PI = $PI")
 }
